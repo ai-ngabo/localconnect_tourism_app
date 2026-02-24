ced49ea4f58522b245b7c4ac81e874134971768e
Alain Ishimwe NGABO <ngaboalan1@gmail.com>
2026-02-24 21:46:42 +0200

    App Theme structure: added on app_theme.dart

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+
+ThemeData lightTheme = ThemeData(
+  primaryColor: AppColors.primaryColor,
+  scaffoldBackgroundColor: AppColors.backgroundColor,
+);
