fe857ee952e5dd8034b76b02fa4f22ccac21e40e
Alain Ishimwe NGABO <ngaboalan1@gmail.com>
2026-02-24 21:38:55 +0200

    App Text styles structure: added on app_text_styles.dart

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const heading = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const body = TextStyle(
+    fontSize: 16,
+  );
+}
