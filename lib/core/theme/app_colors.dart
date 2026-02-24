56ac3920e893360051d3aa5ec8db6ebf71c78b98
Alain Ishimwe NGABO <ngaboalan1@gmail.com>
2026-02-24 21:33:47 +0200

    App colors structure: added on app_colors.dart

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF0A6C5D);
+  static const secondaryColor = Color(0xFFF4A261);
+  static const backgroundColor = Color(0xFFF8F9FA);
+  static const textDark = Color(0xFF212121);
+  static const textLight = Color(0xFFFFFFFF);
+}
