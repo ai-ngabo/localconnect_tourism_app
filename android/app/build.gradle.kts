e4f2718b9df2a20438e2bfffb50baca232303c2d
IbraGold <i.ibrahim@alustudent.com>
2026-03-12 07:25:28 +0200

    Set Android namespace/appId and iOS display name
    Add Android namespace and applicationId in android/app/build.gradle.kts and set android:label in AndroidManifest to "community_touring_rwanda". Update iOS Info.plist to set CFBundleDisplayName to "Community Touring Rwanda" and CFBundleName to "community_touring_rwanda". These changes configure the app package identifiers and user-facing display names for Android and iOS.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 }
 
 android {
+    namespace = "com.example.community_touring_rwanda"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -23,6 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.community_touring_rwanda"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
